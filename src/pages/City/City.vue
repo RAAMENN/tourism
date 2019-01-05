95538da8f6ec8d78241301ec24a4642063ef1d48
raamenn <raamenn92@gmail.com>
2019-01-05 22:57:24 +0800

    finish city list

--- src/pages/City/City.vue
+++ src/pages/City/City.vue
@@ -2,17 +2,23 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <alphabet></alphabet>
   </div>
 </template>
 
 <script type='text/ecmascript-6'>
 import citySearch from './components/Search'
 import cityHeader from './components/Header'
+import cityList from './components/List'
+import alphabet from './components/Alphabet'
 export default {
   name: 'city',
   components: {
     cityHeader,
-    citySearch
+    citySearch,
+    cityList,
+    alphabet
   }
 }
 </script>
