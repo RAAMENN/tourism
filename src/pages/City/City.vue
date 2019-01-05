c79a7b7f476f7c694fdf2b9bb6294a8c84f20da9
raamenn <raamenn92@gmail.com>
2019-01-05 22:02:23 +0800

    add city header

---
+++ src/pages/City/City.vue
@@ -0,0 +1,17 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script type='text/ecmascript-6'>
+import cityHeader from './components/Header'
+export default {
+  name: 'city',
+  components: {
+    cityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
